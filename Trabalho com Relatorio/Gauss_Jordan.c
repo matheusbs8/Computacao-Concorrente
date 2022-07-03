b9612d79ad6c17ba8d5953fff1043a3d63aff60f
matheusbs8 <matheusbs@dcc.ufrj.br>
2022-07-03 14:31:08 -0300

    algoritmos comentados

--- Trabalho com Relatorio/Gauss_Jordan.c
+++ Trabalho com Relatorio/Gauss_Jordan.c
@@ -5,24 +5,23 @@
 #include "timer.h"
 long int size_linha;
 long int size_coluna;
-int nthreads;
-double *A, tempofunction=0, *Coeficientes, *Resultado, *ResultadoGauss, *ResultadoFinal;
-typedef struct{
-   int id;
-   int i; 
-   int j; 
-} tArgs;
+double *A, tempofunction=0, *Coeficientes, *ResultadoArq, *ResultadoGauss, *ResultadoFinal;
+
 FILE *p;
-void estica_encolhe(int indice,double fator){
+
+//operação fundamental que multiplica a linha de uma matriz por um valor especificado no parametro
+void estica_encolhe(int indice, double fator){
     for(int i=0; i<size_coluna; i++)
         A[indice*size_coluna + i] *= fator;  
 }
 
+//operação fundamental que faz uma linha receber o seu valor, somado o valor de outra linha multiplicada por uma constante
 void soma_subtracao(int i_static,int j_static, double fator){
     for(int i=0; i<size_coluna; i++)
         A[i_static*size_coluna + i] -= fator* A[j_static*size_coluna + i];  
 }
 
+//operação fundamental que troca duas linhas da matriz de lugar
 void troca(int i_static, int j_static){
     double aux;
     for(int i=0; i<size_coluna; i++){
@@ -33,6 +32,7 @@ void troca(int i_static, int j_static){
 
 }
 
+//função que percorre a coluna da matriz vasculhando se existe um possivel pivo(número diferente de 0) nela
 int encontra_pivo(int i,int j){
     int i_troca=-1;
     for(int k = i; k<size_linha; k++){
@@ -44,40 +44,22 @@ int encontra_pivo(int i,int j){
     return i_troca;  
 }
 
-void * Zerar (void * arg) {
-   tArgs *args = (tArgs*) arg;
-   for(int k = args->id; k<size_linha; k+=nthreads){
-        if (k!=args->i)
-            soma_subtracao(k, args->i, A[k*size_coluna +args->j]);
+//função responsável por zerar os elementos da coluna, exceto a posição que abriga o pivo unitário da coluna
+//para cumprir esse papel, ela utiliza a função soma_subtração
+void zerar_coluna(int i,int j){
+ 
+    for(int k = 0; k<size_linha; k++){
+        if (k!=i)
+            soma_subtracao(k, i, A[k*size_coluna +j]);
     }
-   
-   pthread_exit(NULL);
-}
-
-void zerar_coluna_concorrente(int i_static,int j_static){
-    pthread_t tid[nthreads];
-    tArgs *args;
-    args = (tArgs*) malloc(sizeof(tArgs)*nthreads);
-   if(args==NULL) {puts("ERRO--malloc"); }
-    for(int i=0; i<nthreads; i++){
-        (args+i)->id = i;
-        (args+i)->i = i_static;
-        (args+i)->j=j_static;
-        if (pthread_create(&tid[i], NULL, Zerar, (void *)(args + i))) 
-            printf("ERRO -- pthread_create\n");
-    }
-    
-    //espera as threads terminarem 
-    for(int i=0; i<nthreads; i++) {
-       if (pthread_join(tid[i], NULL)) 
-          printf("ERRO -- pthread_join\n");
-    }
-    free(args);
  }
+
+//função que executa o gauss Jordan na matriz extraida do arquivo
 void  Gauss_Jordan(){
     
     int i_atual=0;
     int j_atual=0;
+
     while(i_atual<size_linha && j_atual<size_coluna){
        
         int i_troca = encontra_pivo(i_atual, j_atual);
@@ -94,9 +76,8 @@ void  Gauss_Jordan(){
         if(i_atual!=i_troca)
             troca(i_atual, i_troca);
        
-        estica_encolhe(i_atual, 1.0/A[i_atual*size_coluna + j_atual]);
-        
-        zerar_coluna_concorrente(i_atual, j_atual);
+        estica_encolhe(i_atual, 1.0/A[i_atual*size_coluna + j_atual]);  
+        zerar_coluna(i_atual, j_atual);
         
         i_atual+=1;
         j_atual+=1;  
@@ -104,11 +85,13 @@ void  Gauss_Jordan(){
     }
 }
 
-
+//função com o papel de verificar se o resultado obtido pelo gauss jordan, é a solução do sistema
+//verifica se o resultado obtido pela multiplicação é bem próximo do que foi fornecido pelo arquivo 
 int verifica(){
     int aux = 1;
     for(int i=0; i<size_linha; i++){
-        if(abs(Resultado[i]-ResultadoFinal[i])>0.00000001){
+       
+        if(abs(ResultadoArq[i]-ResultadoFinal[i])>0.000000001){
             aux=0;
             break;
         }
@@ -116,8 +99,9 @@ int verifica(){
     return aux;
  }
 
+//função com o papel de realizar a multiplicação dos coeficientes com os valores da variáveis encontrados pelo gauss jordan
+// o reultado encontrado será comparado com o resultado enviado por arquivo
 void ResolveSistema(){
- 
     double aux=0;
     for(int i=0; i<size_linha; i++){
         
@@ -129,12 +113,12 @@ void ResolveSistema(){
     }
 }
 
-
+//função que guarda em uma matriz os coeficientes do sistema, e em um vetor os valores do resultado
 void separa(){
     for(int i=0; i<size_linha; i++){
         for(int j=0; j<size_coluna; j++){
             if(j==size_coluna-1){
-                Resultado[i] =A[i*size_coluna+j]; 
+                ResultadoArq[i] =A[i*size_coluna+j]; 
             }
             else{
                 Coeficientes[i*size_linha + j] =A[i*size_coluna+j];   
@@ -144,6 +128,9 @@ void separa(){
     }  
 }
 
+
+//função responsável de guardar os valores da última coluna da matriz que foi aplicada o algoritmo de gauss Jordan
+//a execução deste processo é necessário pois, na ultima coluna está gurdada a solução do sistema
 void extrairResultado(){
     for(int i=0; i<size_linha; i++){
         ResultadoGauss[i]=A[i*size_coluna + size_coluna-1];
@@ -152,11 +139,7 @@ void extrairResultado(){
 
 
 int main(int argc, char* argv[]){
-    if(argc<2) {
-      printf("Digite: %s <número de threads>\n", argv[0]);
-      return 1;
-   }
-   nthreads = atoi(argv[1]);
+
     p = fopen("Sistema" , "rb");
 
     fread(&size_linha, sizeof(long int), 1, p);
@@ -170,8 +153,8 @@ int main(int argc, char* argv[]){
     Coeficientes  = (double *) malloc(sizeof(double) * size_linha * size_linha);
     if (Coeficientes == NULL) {printf("ERRO--malloc2\n"); return 2;}
 
-    Resultado  = (double *) malloc(sizeof(double) * size_linha);
-    if (Resultado == NULL) {printf("ERRO--malloc3\n"); return 2;}
+    ResultadoArq  = (double *) malloc(sizeof(double) * size_linha);
+    if (ResultadoArq == NULL) {printf("ERRO--malloc3\n"); return 2;}
 
     ResultadoGauss  = (double *) malloc(sizeof(double) * size_linha);
     if (ResultadoGauss == NULL) {printf("ERRO--malloc4\n"); return 2;}
@@ -181,11 +164,9 @@ int main(int argc, char* argv[]){
     separa();
 
     double ini, fim, tempoGauss, tempoS; 
-    
     GET_TIME(ini);
     Gauss_Jordan();
     GET_TIME(fim);
-    
     extrairResultado();
     
     ResolveSistema();
@@ -199,7 +180,7 @@ int main(int argc, char* argv[]){
     free(A);
     free(Coeficientes);
     free(ResultadoFinal);
-    free(Resultado);
+    free(ResultadoArq);
     free(ResultadoGauss);
     return 0;
 }
--- Trabalho com Relatorio/Gauss_Jordan_Concorrente.c
+++ Trabalho com Relatorio/Gauss_Jordan_Concorrente.c
@@ -5,8 +5,13 @@
 #include "timer.h"
 long int size_linha;
 long int size_coluna;
-double *A, tempofunction=0, *Coeficientes, *Resultado, *ResultadoGauss, *ResultadoFinal;
-
+int nthreads;
+double *A, tempofunction=0, *Coeficientes, *ResultadoArq, *ResultadoGauss, *ResultadoFinal;
+typedef struct{
+   int id;
+   int i; 
+   int j; 
+} tArgs;
 FILE *p;
 
 //operação fundamental que multiplica a linha de uma matriz por um valor especificado no parametro
@@ -15,6 +20,7 @@ void estica_encolhe(int indice, double fator){
         A[indice*size_coluna + i] *= fator;  
 }
 
+//operação fundamental que faz uma linha receber o seu valor, somado o valor de outra linha multiplicada por uma constante
 void soma_subtracao(int i_static,int j_static, double fator){
     for(int i=0; i<size_coluna; i++)
         A[i_static*size_coluna + i] -= fator* A[j_static*size_coluna + i];  
@@ -43,19 +49,48 @@ int encontra_pivo(int i,int j){
     return i_troca;  
 }
 
-
-void zerar_coluna(int i,int j){
- 
-    for(int k = 0; k<size_linha; k++){
-        if (k!=i)
-            soma_subtracao(k, i, A[k*size_coluna +j]);
+//função que será executada pelas threads e terão a função de executar a função soma_sutração em uma determinada linha
+void * Zerar (void * arg) {
+   tArgs *args = (tArgs*) arg;
+   for(int k = args->id; k<size_linha; k+=nthreads){
+        if (k!=args->i)
+            soma_subtracao(k, args->i, A[k*size_coluna +args->j]);
     }
+   
+   pthread_exit(NULL);
+}
+
+//função responsável por zerar os elementos da coluna, exceto a posição que abriga o pivo unitário da coluna
+//para cumprir esse papel, ela criará as threads que executarão a função soma_sutração
+void zerar_coluna_concorrente(int i_static,int j_static){
+    pthread_t tid[nthreads];
+    tArgs *args;
+    args = (tArgs*) malloc(sizeof(tArgs)*nthreads);
+   if(args==NULL) {puts("ERRO--malloc"); }
+    for(int i=0; i<nthreads; i++){
+        (args+i)->id = i;
+        (args+i)->i = i_static;
+        (args+i)->j=j_static;
+        if (pthread_create(&tid[i], NULL, Zerar, (void *)(args + i))) 
+            printf("ERRO -- pthread_create\n");
+    }
+    
+    //espera as threads terminarem 
+    for(int i=0; i<nthreads; i++) {
+       if (pthread_join(tid[i], NULL)) 
+          printf("ERRO -- pthread_join\n");
+    }
+    free(args);
  }
 
+
+
+//função que executa o gauss Jordan na matriz extraida do arquivo
 void  Gauss_Jordan(){
     
     int i_atual=0;
     int j_atual=0;
+
     while(i_atual<size_linha && j_atual<size_coluna){
        
         int i_troca = encontra_pivo(i_atual, j_atual);
@@ -73,7 +108,7 @@ void  Gauss_Jordan(){
             troca(i_atual, i_troca);
        
         estica_encolhe(i_atual, 1.0/A[i_atual*size_coluna + j_atual]);  
-        zerar_coluna(i_atual, j_atual);
+        zerar_coluna_concorrente(i_atual, j_atual);
         
         i_atual+=1;
         j_atual+=1;  
@@ -81,13 +116,15 @@ void  Gauss_Jordan(){
     }
 }
 
+
+
 //função com o papel de verificar se o resultado obtido pelo gauss jordan, é a solução do sistema
-//verifica se o resultado obtido pela multiplicação é bem póximo do que foi fornecido pelo arquivo 
+//verifica se o resultado obtido pela multiplicação é bem próximo do que foi fornecido pelo arquivo 
 int verifica(){
     int aux = 1;
     for(int i=0; i<size_linha; i++){
        
-        if(abs(Resultado[i]-ResultadoFinal[i])>0.000000001){
+        if(abs(ResultadoArq[i]-ResultadoFinal[i])>0.000000001){
             aux=0;
             break;
         }
@@ -109,12 +146,12 @@ void ResolveSistema(){
     }
 }
 
-//função que guarda em uma matriz os coeficientes do sistema
+//função que guarda em uma matriz os coeficientes do sistema, e em um vetor os valores do resultado
 void separa(){
     for(int i=0; i<size_linha; i++){
         for(int j=0; j<size_coluna; j++){
             if(j==size_coluna-1){
-                Resultado[i] =A[i*size_coluna+j]; 
+                ResultadoArq[i] =A[i*size_coluna+j]; 
             }
             else{
                 Coeficientes[i*size_linha + j] =A[i*size_coluna+j];   
@@ -124,6 +161,9 @@ void separa(){
     }  
 }
 
+
+//função responsável de guardar os valores da última coluna da matriz que foi aplicada o algoritmo de gauss Jordan
+//a execução deste processo é necessário pois, na ultima coluna está gurdada a solução do sistema
 void extrairResultado(){
     for(int i=0; i<size_linha; i++){
         ResultadoGauss[i]=A[i*size_coluna + size_coluna-1];
@@ -131,8 +171,13 @@ void extrairResultado(){
 }
 
 
-int main(int argc, char* argv[]){
 
+int main(int argc, char* argv[]){
+    if(argc<2) {
+      printf("Digite: %s <número de threads>\n", argv[0]);
+      return 1;
+   }
+   nthreads = atoi(argv[1]);
     p = fopen("Sistema" , "rb");
 
     fread(&size_linha, sizeof(long int), 1, p);
@@ -146,8 +191,8 @@ int main(int argc, char* argv[]){
     Coeficientes  = (double *) malloc(sizeof(double) * size_linha * size_linha);
     if (Coeficientes == NULL) {printf("ERRO--malloc2\n"); return 2;}
 
-    Resultado  = (double *) malloc(sizeof(double) * size_linha);
-    if (Resultado == NULL) {printf("ERRO--malloc3\n"); return 2;}
+    ResultadoArq  = (double *) malloc(sizeof(double) * size_linha);
+    if (ResultadoArq == NULL) {printf("ERRO--malloc3\n"); return 2;}
 
     ResultadoGauss  = (double *) malloc(sizeof(double) * size_linha);
     if (ResultadoGauss == NULL) {printf("ERRO--malloc4\n"); return 2;}
@@ -157,9 +202,11 @@ int main(int argc, char* argv[]){
     separa();
 
     double ini, fim, tempoGauss, tempoS; 
+    
     GET_TIME(ini);
     Gauss_Jordan();
     GET_TIME(fim);
+    
     extrairResultado();
     
     ResolveSistema();
@@ -173,7 +220,7 @@ int main(int argc, char* argv[]){
     free(A);
     free(Coeficientes);
     free(ResultadoFinal);
-    free(Resultado);
+    free(ResultadoArq);
     free(ResultadoGauss);
     return 0;
 }
